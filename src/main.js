a57db95641f81f63832e0bf57557699a71cd7c2d
bao-yu-yu <72684109+bao-yu-yu@users.noreply.github.com>
2021-07-01 16:51:18 +0800

    '更新202107011651'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index.js"
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const app = new Vue({
     router,
     render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
